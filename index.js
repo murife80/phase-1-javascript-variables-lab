78724e1f7cb84c61de8b21ccdd4e3d767380ee5b
melvin muchui <melvin.muchui1@student.moringaschool.com>
2025-03-11 11:05:00 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution in this file!
+let companyName = 'Scuber';
+describe('companyName', function () {
+  it('is set as Scuber', function () {
+    expect(companyName).to.equal('Scuber');
+  });
+
+  it('is defined as a const', function () {
+    expect(js).to.match(/const companyName/, "Expected companyName to be a const");
+  });
+}
+
+
+
+
